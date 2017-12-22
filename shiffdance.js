cb180d0ffbf78cc238de50b7919e15712262e740
LankySmurph <lankysmurph@smurphey.com>
2017-12-21 19:02:45 -0600

    Add files via upload

--- shiffdance.js
+++ shiffdance.js
@@ -13,7 +13,7 @@ let imgs = document.getElementsByTagName('img');
 
 for (let imgElt of imgs) {
   let r = Math.floor(Math.random() * filenames.length);
-  let file = 'shifdance/' + filenames[r];
+  let file = 'shiffdance/' + filenames[r];
   let url = chrome.extension.getURL(file);
   imgElt.src = url;
   console.log(url);
